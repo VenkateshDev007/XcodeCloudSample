7ee82b522894a678fdc6a48e3a654f501236da43
Venkatesh Polepalli <Venkatesh.polepalli@innovasolutions.com>
2025-09-26 12:48:01 +0530

    testcases updated

--- XcodeCloudSample/XcodeCloudSampleTests/XcodeCloudSampleTests.swift
+++ XcodeCloudSample/XcodeCloudSampleTests/XcodeCloudSampleTests.swift
@@ -11,7 +11,13 @@ import Testing
 struct XcodeCloudSampleTests {
 
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let viewModel = CounterViewModel()
+        
+        // Act
+        viewModel.increment()
+        
+        // Assert
+        #expect(viewModel.count == 1)
     }
 
 }

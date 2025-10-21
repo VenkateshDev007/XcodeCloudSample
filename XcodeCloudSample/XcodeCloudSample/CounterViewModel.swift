91461064f912f8971b430a4c98c2c06c2fe3e5fc
Venkatesh Polepalli <Venkatesh.polepalli@innovasolutions.com>
2025-10-21 12:45:59 +0530

    Added Comments

--- XcodeCloudSample/XcodeCloudSample/CounterViewModel.swift
+++ XcodeCloudSample/XcodeCloudSample/CounterViewModel.swift
@@ -10,10 +10,12 @@ import Foundation
 class CounterViewModel: ObservableObject {
     @Published var count: Int = 0
 
+    //MARK: - increment count
     func increment() {
         count += 1
     }
     
+    //MARK: - deccrement count
     func decrement() {
         count -= 1
     }
